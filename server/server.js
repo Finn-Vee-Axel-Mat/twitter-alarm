e8a66fee7ecd7b1d54a5021688ee355e405db811
Vy Vu <bella.vnpv@gmail.com>
2022-03-02 17:32:21 +0100

    setup mongodb server for account authentication

---
+++ server/server.js
@@ -0,0 +1,30 @@
+require("./models/User");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri =
+  "mongodb+srv://admin:DgF6BChK9Gbw92Uk@cluster0.6z61n.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
