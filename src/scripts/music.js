8ab5c0e89dd8377778542fcf4161789f5b3342cc
= <petitcroco>
2022-03-10 22:02:19 +0100

    settings pages, local volume % & bool for email variable

--- src/scripts/music.js
+++ src/scripts/music.js
@@ -1,9 +1,11 @@
 export const playNoisyAlarm = () => {
 	var audio = new Audio('https://lasonotheque.org/UPLOAD/mp3/0161.mp3');
+	audio.volume = (localStorage.getItem("audioAlert")/100);
 	audio.play();
 }
 
 export const playGentleAlarm = () => {
 	var audio = new Audio('https://lasonotheque.org/UPLOAD/mp3/0840.mp3');
+	audio.volume = (localStorage.getItem("audioAlert")/100);
 	audio.play();
 }
